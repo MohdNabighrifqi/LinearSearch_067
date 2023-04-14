bcb6394ff0dbdbb510804ba534eb844ea30aa01c
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-04-14 15:18:06 +0700

    masukan comparisions ++

--- LinearSearch_067/LinearSearch_067.cpp
+++ LinearSearch_067/LinearSearch_067.cpp
@@ -40,7 +40,15 @@ void linesearch()
 		cin >> item;
 		comparisons = 0;
 		for (i + 0; i < n; i++) //langkah 2, 3, dan 4
-	
+		{
+			comparisons++;
+			if (Arr[i] == item)// Langkah 5A found 
+			{
+				cout << "\n" << item << "found at posisition" << (i + 1) << endl;
+
+			}
+
+		
 
 	
 
