ef4e27c52cd816732229a9b5911c5f04b500d9a2
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-04-14 14:48:47 +0700

    memasukan while

--- LinearSearch_067/LinearSearch_067.cpp
+++ LinearSearch_067/LinearSearch_067.cpp
@@ -5,3 +5,16 @@ int Arr[20]; //Array to be seaeched
 int n; //number in the elemenet of the array
 int i; // index of the array element
 
+void input()
+{
+	while (true)
+	{
+		cout << "enter the number of element in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else cout << "\nArray should have minimum 1 and maximum 20 element.\n\n";
+	}
+
+	
+}
